b7f33b44f2f42c18c0763c841f8ee4a2480eb299
Alexey <support@uvviewsoft.com>
2021-02-13 22:35:09 +0300

    i18n

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
 from cudatext import *
 from .proc_hash import *
 
+from cudax_lib import get_translation
+_   = get_translation(__file__)  # I18N
+
 
 class Command:
     id_dlg = None
@@ -12,7 +15,7 @@ class Command:
         self.data = data
         self.is_file = is_file
 
-        caption = '&Hash -- of '+('file' if is_file else 'string')+':'
+        caption = _('&Hash -- of {}:')-format(_('file') if is_file else _('string'))
         dlg_proc(id_dlg, DLG_CTL_PROP_SET, name='label_hash', prop={'cap': caption} )
 
         index = int(dlg_proc(id_dlg, DLG_CTL_PROP_GET, name='combo_type')['val'])
@@ -58,7 +61,7 @@ class Command:
         prop = dlg_proc(id_dlg, DLG_CTL_PROP_GET, name='edit_hash')
         s = prop['val']
         app_proc(PROC_SET_CLIP, s)
-        msg_status('Hash copied to clipboard')
+        msg_status(_('Hash copied to clipboard'))
 
 
     def callback_btn_verify(self, id_dlg, id_ctl, data='', info=''):
@@ -69,17 +72,17 @@ class Command:
         s2 = prop2['val']
         ok = bool(s1) and (s1==s2)
 
-        caption = 'Verified' if ok else '?'
+        caption = _('Verified') if ok else '?'
         dlg_proc(id_dlg, DLG_CTL_PROP_SET, name='label_verify_res', prop={'cap':caption} )
 
 
     def init_dlg(self):
 
         h=dlg_proc(0, DLG_CREATE)
-        dlg_proc(h, DLG_PROP_SET, prop={'cap':'Hash Generator', 'w':670, 'h':320 })
+        dlg_proc(h, DLG_PROP_SET, prop={'cap':_('Hash Generator'), 'w':670, 'h':320 })
 
         n=dlg_proc(h, DLG_CTL_ADD, 'label')
-        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'label_type', 'cap':'Hash &type:', 'x':6, 'y':10, 'w':120 } )
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'label_type', 'cap':_('Hash &type:'), 'x':6, 'y':10, 'w':120 } )
 
         n=dlg_proc(h, DLG_CTL_ADD, 'combo_ro')
         dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'combo_type', 'x':120, 'y':6, 'w':150,
@@ -90,40 +93,40 @@ class Command:
            } )
 
         n=dlg_proc(h, DLG_CTL_ADD, 'label')
-        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'chk_from_str', 'cap':'Calculate hash from &string:', 'x':6, 'y':40, 'w':120 } )
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'chk_from_str', 'cap':_('Calculate hash from &string:'), 'x':6, 'y':40, 'w':120 } )
 
         n=dlg_proc(h, DLG_CTL_ADD, 'edit')
         dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'edit_string', 'x':6, 'y':60, 'w':550 } )
 
         n=dlg_proc(h, DLG_CTL_ADD, 'button')
-        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_string', 'cap':'C&alculate', 'x':560, 'y':60, 'w':100,
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_string', 'cap':_('C&alculate'), 'x':560, 'y':60, 'w':100,
           'on_change': self.callback_btn_string } )
 
 
         n=dlg_proc(h, DLG_CTL_ADD, 'label')
-        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'chk_from_file', 'cap':'Calculate hash from &file:', 'x':6, 'y':95, 'w':120 } )
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'chk_from_file', 'cap':_('Calculate hash from &file:'), 'x':6, 'y':95, 'w':120 } )
 
         n=dlg_proc(h, DLG_CTL_ADD, 'edit')
         dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'edit_file', 'props':(True,False,True), 'x':6, 'y':115, 'w':550 } )
 
         n=dlg_proc(h, DLG_CTL_ADD, 'button')
-        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_file', 'cap':'&Browse...', 'x':560, 'y':115, 'w':100,
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_file', 'cap':_('&Browse...'), 'x':560, 'y':115, 'w':100,
           'on_change': self.callback_btn_file } )
 
 
         n=dlg_proc(h, DLG_CTL_ADD, 'label')
-        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'label_hash', 'cap':'&Hash value:', 'x':6, 'y':150, 'w':120 } )
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'label_hash', 'cap':_('&Hash value:'), 'x':6, 'y':150, 'w':120 } )
 
         n=dlg_proc(h, DLG_CTL_ADD, 'edit')
         dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'edit_hash', 'props':(True,True,True), 'x':6, 'y':170, 'w':550 } )
 
         n=dlg_proc(h, DLG_CTL_ADD, 'button')
-        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_copy', 'cap':'&Copy', 'x':560, 'y':170, 'w':100,
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_copy', 'cap':_('&Copy'), 'x':560, 'y':170, 'w':100,
           'on_change': self.callback_btn_copy } )
 
 
         n=dlg_proc(h, DLG_CTL_ADD, 'label')
-        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'label_varify', 'cap':'&Enter hash to verify:', 'x':6, 'y':205, 'w':120 } )
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'label_varify', 'cap':_('&Enter hash to verify:'), 'x':6, 'y':205, 'w':120 } )
 
         n=dlg_proc(h, DLG_CTL_ADD, 'label')
         dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'label_verify_res', 'cap':'?', 'props':True, 'x':100, 'y':205, 'w':355 } )
@@ -132,12 +135,12 @@ class Command:
         dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'edit_verify', 'props':(False,True,True), 'x':6, 'y':225, 'w':550 } )
 
         n=dlg_proc(h, DLG_CTL_ADD, 'button')
-        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_verify', 'cap':'&Verify', 'x':560, 'y':225, 'w':100,
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_verify', 'cap':_('&Verify'), 'x':560, 'y':225, 'w':100,
           'on_change': self.callback_btn_verify } )
 
 
         n=dlg_proc(h, DLG_CTL_ADD, 'button')
-        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_close', 'cap':'Close', 'x':560, 'y':290, 'w':100,
+        dlg_proc(h, DLG_CTL_PROP_SET, index=n, prop={'name': 'btn_close', 'cap':_('Close'), 'x':560, 'y':290, 'w':100,
           'on_change': self.callback_btn_close } )
 
         return h
